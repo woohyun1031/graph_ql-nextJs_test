aae930375cd9d25bd8742ae2bd22c6f17e1e3e3e
KimWooHyun <ktkwhms3@gmail.com>
2022-11-05 22:40:09 +0900

    Feat: notion api

---
+++ notion-example/test.js
@@ -0,0 +1,39 @@
+import { Client } from "@notionhq/client";
+
+const notion = new Client({ auth: process.env.NOTION_KEY });
+const databaseId = process.env.NOTION_DATABASE_ID;
+
+export async function addItem(text) {
+  try {
+    const response = await notion.pages.create({
+      parent: { database_id: databaseId },
+      properties: {
+        title: {
+          title: [
+            {
+              text: {
+                content: text,
+              },
+            },
+          ],
+        },
+      },
+    });
+    console.log(response);
+    console.log("Success! Entry added.");
+  } catch (error) {
+    console.error(error.body);
+  }
+}
+
+export async function getItem() {
+  try {
+    const response = await notion.databases.query({ database_id: databaseId });
+    console.log(response);
+    console.log("Success! Entry added.");
+  } catch (error) {
+    console.error(error.body);
+  }
+}
+
+//addItem("Yurts in Big Sur, California");
